311778c80ee009afec6c9aa85dec51b3f19da1ed
K. M. Rajib Faysal <kmrajibfaysal@gmail.com>
2022-03-14 23:11:59 +0600

    adding practice files

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,38 @@ function App() {
         <div>
             <h1>Hello world!</h1>
             <h1>Hi there whats up ki khobor</h1>
+            <div>
+                <h1>ola</h1>
+                <p>ki re ki khobor din kal kmn jasce tor!</p>
+                <p>
+                    kire ostad dinkal mone hoy seirokom jasce taina? eto valo to jaoar kotha na,
+                    shon besi din jay na valo hoia jays kintu buncis
+                </p>
+                <p>
+                    er poreo jodi dinal emn e chalas taiel ki hobe naki bolto to samne jedin asbi
+                    sob baindha nia asbi kintu bujcis? er por kisu jinis tor jonno besi kore rkehe
+                    dite hobe ar tar por sob bese dia eka eka thakte hobe naile code kintu
+                    tKcgSeiCHRSBdXoIoqX1pgcKK mot bs
+                </p>
+                <p>
+                    shono sobsomoy besi kisu uase kora kintu subidhar na bujsco majhe majhe besi
+                    kisu use korte gele onk kisu gossa dite hoy ar erporeo besi kisu dekhte na
+                    chaile sob kisu batil hoa jabe kintu
+                </p>
+                <p>
+                    What is the time to recover this system brohter you should know how to build up
+                    the overall system for better performance and so you should pay attenstion to
+                    the society which everything is going to held this vampire theory and so on you
+                    know what you should definately do some special work to put some extra effort to
+                    your vast majority system and so on!
+                </p>
+                <p>tar</p>
+                <p>
+                    definately boss what should i do and what should we not to do then remember this
+                    theory bs
+                </p>
+            </div>
+            bs
         </div>
     );
 }
